083252126f4b6ce9135cab65d11ec7503fe39944
shirajuki <shirajuki00@gmail.com>
2021-06-07 00:10:02 +0200

    feat: adds typescript, and gitignore files

--- src/App.svelte
+++ src/App.svelte
@@ -1,55 +1,97 @@
-<script>
+<script lang="ts">
 	import { onMount } from 'svelte';
 
+	class Animal {
+		x: number;
+		y: number;
+		width: number;
+		height: number;
+		color: string;
+		constructor(x:number, y:number, width:number, height:number, color:string) {
+			this.x = x;
+			this.y = y;
+			this.width = width;
+			this.height = height;
+			this.color = color;
+		}
+		draw(ctx) {
+			ctx.beginPath();
+			ctx.fillStyle = this.color;
+			ctx.rect(this.x, this.y, this.width, this.height);
+			ctx.fill();
+
+			ctx.beginPath();
+			ctx.arc(this.x, this.y, this.width / 2, 0, 2 * Math.PI);
+			ctx.fill();
+		}
+	}
+	class Player extends Animal {
+		is_sexy = false;
+		meow() {
+			console.log('Meow!')
+		}
+	}
 	let canvas;
+	const player = new Player(100, 100, 50, 50, "red");
 
 	onMount(() => {
 		const ctx = canvas.getContext('2d');
-		let frame;
+		let frame; // AnimationFrame cancel on unmount / exit
+		let secondsPassed, oldTimeStamp, fps; // FPS
+		const gameLoop = (timeStamp) => {
+			frame = requestAnimationFrame(gameLoop);
+			// Calculate the number of seconds passed since the last frame
+			secondsPassed = (timeStamp - oldTimeStamp) / 1000;
+			oldTimeStamp = timeStamp;
+			// Calculate fps
+			fps = Math.round(1 / secondsPassed);
 
-		(function loop() {
-			frame = requestAnimationFrame(loop);
+			if (ctx) {
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+				ctx.beginPath();
+				ctx.fillStyle = "#000";
+				ctx.rect(200,200,20,20);
+				ctx.fill();
 
-			const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-
-			for (let p = 0; p < imageData.data.length; p += 4) {
-				const i = p / 4;
-				const x = i % canvas.width;
-				const y = i / canvas.height >>> 0;
-
-				const t = window.performance.now();
-
-				const r = 64 + (128 * x / canvas.width) + (64 * Math.sin(t / 1000));
-				const g = 64 + (128 * y / canvas.height) + (64 * Math.cos(t / 1400));
-				const b = 128;
-
-				imageData.data[p + 0] = r;
-				imageData.data[p + 1] = g;
-				imageData.data[p + 2] = b;
-				imageData.data[p + 3] = 255;
+				// Draw number to the screen
+				ctx.font = '25px Arial';
+				ctx.fillStyle = 'black';
+				ctx.fillText("FPS: " + fps, canvas.width - 100, canvas.height - 10);
+				player.draw(ctx);
 			}
-
-			ctx.putImageData(imageData, 0, 0);
-		}());
-
+		}
+		requestAnimationFrame(gameLoop);
 		return () => {
 			cancelAnimationFrame(frame);
 		};
 	});
+
+	document.onkeydown = (event) => {
+		if (event.key === 'ArrowRight')
+			player.x += 10;
+		else if (event.key === 'ArrowUp')
+			player.y -= 10;
+		else if (event.key === 'ArrowLeft')
+			player.x -= 10;
+		else if (event.key === 'ArrowDown')
+			player.y += 10;
+		event.preventDefault();
+	}
 </script>
 
 <canvas
 	bind:this={canvas}
-	width={32}
-	height={32}
+	width={1024}
+	height={600}
 ></canvas>
 
 <style>
+	* { box-sizing: border-box; }
 	canvas {
-		width: 100%;
-		height: 100%;
-		background-color: #666;
-		-webkit-mask: url(/svelte-logo-mask.svg) 50% 50% no-repeat;
-		mask: url(/svelte-logo-mask.svg) 50% 50% no-repeat;
+		background-color: #eee;
+		position: absolute;
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%, -50%);
 	}
 </style>
