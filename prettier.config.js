088016a3d23cb2d969164b0ec4b25603a71c7365
shirajuki <shirajuki00@gmail.com>
2021-06-07 20:27:06 +0200

    chore: fix prettier for small window

--- prettier.config.js
+++ prettier.config.js
@@ -2,9 +2,9 @@ module.exports = {
   tabWidth: 2,
   semi: true,
   singleQuote: true,
-  printWidth: 100,
-  plugins: ["prettier-plugin-svelte"],
-  svelteSortOrder: "styles-scripts-markup",
+  printWidth: 80,
+  plugins: ['prettier-plugin-svelte'],
+  svelteSortOrder: 'styles-scripts-markup',
   svelteStrictMode: false,
   svelteBracketNewLine: true,
 };
